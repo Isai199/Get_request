05f2ba95d99c5504fbe02f3c9b8a12ab9cc12bd3
H_ISA <h_isai_gomez@hotmail.com>
2022-04-04 13:59:30 -0500

    Se hiso una modificacion al codido, para mostrar una lista en un textview los pokemon, recividos de la api

--- app/src/main/java/com/get_request/MainActivity.kt
+++ app/src/main/java/com/get_request/MainActivity.kt
@@ -40,12 +40,31 @@ class MainActivity : AppCompatActivity() {
         val jsonRequest = JsonObjectRequest(url , Response.Listener<JSONObject>{ response ->
             //response
             Log.i("JSONRESPONSE", response.getJSONArray("results").toString())
+
+
+
+            binding.rvListPokemon.adapter = PokemonAdapter(response.getJSONArray("results"))
+
+
         }, Response.ErrorListener { error ->
             //error.messge
             Log.w("JSONRESPONSE",error.message as String)
         })
 
         queue.add(jsonRequest)
+
+        /*val fakeVideoData: Array<JSONObject> =  arrayOf(
+            JSONObject("{\"title\": \"Audifono inalambricos sony\", \"price\": \"$ 1,008\", \"meses\": \"en 12x $ 84MSl\", \"manana\": \"llega gratis mañana\", \"vendedor\": \"vendido por MAFE Distribuciones\"}"),
+            JSONObject("{\"title\": \"Control inalambricos ps4\", \"price\": \"$ 1,400\", \"meses\": \"en 12x $ 84MSl\", \"manana\": \"llega gratis mañana\", \"vendedor\": \"vendido por MAFE Distribuciones\"}"),
+            JSONObject("{\"title\": \"Tv samsung\", \"price\": \"$ 5,999\", \"meses\": \"en 12x $ 84MSl\", \"manana\": \"llega gratis mañana\", \"vendedor\": \"vendido por MAFE Distribuciones\"}"),
+            JSONObject("{\"title\": \"Playera nike\", \"price\": \"$ 1,967\", \"meses\": \"en 12x $ 84MSl\", \"manana\": \"llega gratis mañana\", \"vendedor\": \"vendido por MAFE Distribuciones\"}"),
+            JSONObject("{\"title\": \"Telefono xiaomi\", \"price\": \"$ 4,300\", \"meses\": \"en 12x $ 84MSl\", \"manana\": \"llega gratis mañana\", \"vendedor\": \"vendido por MAFE Distribuciones\"}")
+
+        )
+
+
+        binding.rvListPokemon.adapter = PokemonAdapter(fakeVideoData)*/
+
 
     }
 
